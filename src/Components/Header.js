e38222dcd2b665e4e7dfe76c676bf32166d88531
ufuk bal <balufuk91@gmail.com>
2021-07-06 14:06:13 +0300

    refactor: Fix hardcoded values with context

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { useEffect } from 'react';
+import { useGlobalContext } from '../context';
+
 import '../Styles/Header.scss';
 import '../Styles/Event.scss';
 
 function Header() {
+  const { data } = useGlobalContext();
+
+  const title = data[0];
   useEffect(() => {
     const header = document.getElementById('myHeader');
     const sticky = header.offsetTop;
@@ -24,16 +29,16 @@ function Header() {
       <header>Header Container</header>
       <header id='myHeader' className='header'>
         <article className='grid-container'>
-          <div className='grid-child title'>Event Count: 3000</div>
+          <div className='grid-child title'>Event Count: {data.length}</div>
           <div className='grid-child'></div>
-          <div className='grid-child'>1</div>
-          <div className='grid-child'>x</div>
-          <div className='grid-child'>2</div>
-          <div className='grid-child'>Alt</div>
-          <div className='grid-child'>Ust</div>
-          <div className='grid-child'>1-X</div>
-          <div className='grid-child'>1-2</div>
-          <div className='grid-child'>x-2</div>
+          <div className='grid-child'>{title.name1}</div>
+          <div className='grid-child'>{title.nameX}</div>
+          <div className='grid-child'>{title.name2 || 2}</div>
+          <div className='grid-child'>{title.nameAlt}</div>
+          <div className='grid-child'>{title.nameUst}</div>
+          <div className='grid-child'>{title.name1X}</div>
+          <div className='grid-child'>{title.name12}</div>
+          <div className='grid-child'>{title.nameX2}</div>
         </article>
       </header>
     </div>
